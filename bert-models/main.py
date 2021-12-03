7c2098d596914e887750709348137b6b0156cd8c
Talita <talita.anthonio@yahoo.com>
2021-12-03 15:49:56 +0100

    make batches.

--- bert-models/main.py
+++ bert-models/main.py
@@ -1,5 +1,7 @@
 from data_preprocessing import merge_data 
 from torchtext.legacy import data 
+from transformers import BertTokenizer, BertForSequenceClassification
+
 
 PathToTrainLabels = "../data/ClarificationTask_TrainLabels_Sep23.tsv"
 PathToTrainData = "../data/ClarificationTask_TrainData_Sep23.tsv"
@@ -13,13 +15,19 @@ development_df = merge_data(PathToDevData, PathToDevLabels)
 train_df.to_csv("./data/train.csv", index=False)
 development_df.to_csv("./data/dev.csv", index=False)
 
-
 ids = data.Field()
 version = data.Field()
 label = data.Field()
 
 #     merged_df_dict = {"ids": [], "version": [], "label": []}
 fields = {'ids': ('ids', ids), 'version': ('version', version), 'label': ('label', label)}
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Model parameter
+PAD_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+UNK_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.unk_token)
 
 
 train_data, valid_data, test_data = data.TabularDataset.splits(
@@ -33,3 +41,9 @@ train_data, valid_data, test_data = data.TabularDataset.splits(
 )
 
 print(vars(train_data[0]))
+
+
+train_iter = data.BucketIterator(train_data, batch_size=16, sort_key=lambda x: len(version.text), train=True, sort=True, sort_within_batch=True)
+valid_iter = data.BucketIterator(valid_data, batch_size=16, sort_key=lambda x: len(version.text), train=True, sort=True, sort_within_batch=True)
+test_iter = data.Iterator(test_data, batch_size=16, train=False, shuffle=False, sort=False)
+
