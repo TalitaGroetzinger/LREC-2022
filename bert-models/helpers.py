0ea322d116cebb94b8e6c1269ac1d4bf04e45c18
Talita <talita.anthonio@yahoo.com>
2021-12-06 12:28:44 +0100

    stage current version.

---
+++ bert-models/helpers.py
@@ -0,0 +1,41 @@
+from transformers import BertTokenizer, BertForSequenceClassification
+import pdb 
+import torch 
+
+
+def categorical_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    top_pred = preds.argmax(1, keepdim = True)
+    correct = top_pred.eq(y.view_as(top_pred)).sum()
+    acc = correct.float() / y.shape[0]
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+
+    for batch in iterator:
+        
+        
+        optimizer.zero_grad()
+        
+        predictions = model(batch.version)
+        label = batch.label.type(torch.LongTensor)
+        loss = criterion(predictions, label)
+        
+        acc = categorical_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
--- bert-models/main.py
+++ bert-models/main.py
@@ -1,7 +1,15 @@
 from data_preprocessing import merge_data 
+import torch 
 from torchtext.legacy import data 
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer
 import pdb 
+from torch.nn import BCELoss, CrossEntropyLoss
+from transformers import BertTokenizer, BertModel
+from models import BERTClassification
+import torch.optim as optim 
+from helpers import train 
+
+bert = BertModel.from_pretrained('bert-base-uncased')
 
 
 # Dataset reading paths. 
@@ -9,16 +17,31 @@ PathToTrainLabels = "../data/ClarificationTask_TrainLabels_Sep23.tsv"
 PathToTrainData = "../data/ClarificationTask_TrainData_Sep23.tsv"
 PathToDevLabels = "../data/ClarificationTask_DevLabels_Oct22a.tsv"
 PathToDevData = "../data/ClarificationTask_DevData_Oct22a.tsv"
-ids = data.Field()
-version = data.Field()
-label = data.Field()
-fields = {'ids': ('ids', ids), 'version': ('version', version), 'label': ('label', label)}
+
+
+
 
 
 # Model parameters 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 PAD_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
 UNK_INDEX = tokenizer.convert_tokens_to_ids(tokenizer.unk_token)
+MAX_SEQ_LEN = 100
+
+
+# set sequential = False, those fields are not texts. 
+ids = data.Field()
+label = data.Field(sequential=False, use_vocab=False, batch_first=True, dtype=torch.float)
+#label = data.LabelField(dtype=torch.float)
+# set use_vocab=False to use own decoder 
+version = data.Field(use_vocab=False, tokenize=tokenizer.encode, lower=False, include_lengths=False, batch_first=True,
+                   fix_length=MAX_SEQ_LEN, pad_token=PAD_INDEX, unk_token=UNK_INDEX)
+ids.build_vocab()
+#label.build_vocab()
+version.build_vocab()
+
+fields = {'ids': ('ids', ids), 'version': ('version', version), 'label': ('label', label)}
 
 
 def read_data(): 
@@ -38,14 +61,14 @@ def read_data():
                                             fields = fields,
                                             skip_header = False,
     )
-
+    #label.build_vocab(train_data) 
     print("Train instances:", len(train_data)) 
     print("Dev instances:", len(valid_data))
-    train_iter = data.BucketIterator(train_data, batch_size=16, sort_key=lambda x: len(version.text), train=True, sort=True, sort_within_batch=True)
-    valid_iter = data.BucketIterator(valid_data, batch_size=16, sort_key=lambda x: len(version.text), train=True, sort=True, sort_within_batch=True)
+    train_iter = data.BucketIterator(train_data, batch_size=16, sort_key=lambda x: len(x.version), train=True, sort=True, sort_within_batch=True)
+    valid_iter = data.BucketIterator(valid_data, batch_size=16, sort_key=lambda x: len(x.version), train=True, sort=True, sort_within_batch=True)
+    
     test_iter = data.Iterator(test_data, batch_size=16, train=False, shuffle=False, sort=False)
     return train_iter, valid_iter, test_iter
-
 
 
 def main(): 
@@ -53,6 +76,34 @@ def main():
     # read data and return buckets 
     train_iter, valid_iter, test_iter = read_data()
     
+    # create instance of the model 
+    HIDDEN_DIM = 256
+    OUTPUT_DIM = 3
+    N_LAYERS = 2
+    BIDIRECTIONAL = True
+    DROPOUT = 0.25
 
+    
 
+    model = BERTClassification(bert,
+                            HIDDEN_DIM,
+                            OUTPUT_DIM,
+                            N_LAYERS,
+                            BIDIRECTIONAL,
+                            DROPOUT)
+    
+
+    optimizer = optim.Adam(model.parameters())
+
+    criterion = CrossEntropyLoss()
+
+    model = model.to(device)
+    criterion = criterion.to(device)
+    
+    N_EPOCHS = 5
+    best_valid_loss = float('inf')
+    for epoch in range(N_EPOCHS):
+        train_loss, train_acc = train(model, train_iter, optimizer, criterion)
+        print(train_loss, train_acc)
+      
 main()
