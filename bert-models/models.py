c47ef26a58b48c252dcb59ed691d9fd6851ac258
Talita <talita.anthonio@yahoo.com>
2021-12-07 18:47:01 +0100

    add lstm.

--- bert-models/models.py
+++ bert-models/models.py
@@ -1,6 +1,7 @@
 from transformers import BertTokenizer, BertModel
 import torch.nn as nn
 import torch 
+from torch.autograd import Variable
 
 class BERTClassification(nn.Module):
     def __init__(self,
@@ -9,20 +10,29 @@ class BERTClassification(nn.Module):
                  output_dim,
                  n_layers,
                  bidirectional,
-                 dropout):
+                 dropout, LSTM=True):
         
         super().__init__()
         
         self.bert = bert
+        self.lstm = LSTM 
         
         embedding_dim = bert.config.to_dict()['hidden_size']
         
-        self.rnn = nn.GRU(embedding_dim,
+        if self.LSTM: 
+            self.rnn = nn.LSTM(embedding_dim,
                           hidden_dim,
                           num_layers = n_layers,
                           bidirectional = bidirectional,
                           batch_first = True,
-                          dropout = 0 if n_layers < 2 else dropout)
+                          dropout = dropout) 
+        else: 
+            self.rnn = nn.GRU(embedding_dim,
+                            hidden_dim,
+                            num_layers = n_layers,
+                            bidirectional = bidirectional,
+                            batch_first = True,
+                            dropout = 0 if n_layers < 2 else dropout)
         
         self.out = nn.Linear(hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
         
@@ -35,15 +45,22 @@ class BERTClassification(nn.Module):
         embedded = self.bert(text)[0]
                 
         #embedded = [batch size, sent len, emb dim]
-        
-        _, hidden = self.rnn(embedded)
-        
-        #hidden = [n layers * n directions, batch size, emb dim]
-        
-        if self.rnn.bidirectional:
-            hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
-        else:
-            hidden = self.dropout(hidden[-1,:,:])
+
+        if self.LSTM: 
+            h_0 = Variable(torch.randn(self.n_layers*2, embedded.size()[0], self.hidden_dim)).cuda()
+            c_0 = Variable(torch.randn(self.n_layers*2, embedded.size()[0], self.hidden_dim)).cuda()
+            _, hidden = self.rnn(embedded, (h_0, c_0)) 
+
+
+        else:   
+            _, hidden = self.rnn(embedded)
+            
+            #hidden = [n layers * n directions, batch size, emb dim]
+            
+            if self.rnn.bidirectional:
+                hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
+            else:
+                hidden = self.dropout(hidden[-1,:,:])
                 
         #hidden = [batch size, hid dim]
         
